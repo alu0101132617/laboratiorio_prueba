916f6e429ab6accef5b6f0303ae9491a5a6e761c
Alba Morales <alu0101132617@ull.edu.es>
2025-10-30 12:28:51 +0000

    Añadiendo fallos

--- src/main/java/es/ull/esit/app/App.java
+++ src/main/java/es/ull/esit/app/App.java
@@ -4,10 +4,13 @@ package es.ull.esit.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println( "This is a sample Java application." );
+        # Adding errors for testing purposes in sonarQube
+        int a = 5;
+        int b = 0;
+        int c = a / b; // This will cause a division by zero error
     }
 }
